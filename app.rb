d57088fc86905babdc86c7957c6c327e9344e2b3
Arpan Patel <113603155+APatel-AI@users.noreply.github.com>
2024-02-19 16:03:20 +0000

    created the intital template for routes

--- app.rb
+++ app.rb
@@ -7,3 +7,33 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+
+  erb(:square)
+  
+end
+
+get("/square/results") do
+
+  erb(:square_results)
+  
+end
+
+get("/square_root/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/random/new") do
+  ""
+end
+
+get("/payment/new") do
+end
